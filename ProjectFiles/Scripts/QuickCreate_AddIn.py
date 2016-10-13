f86ad47c4f7fd819149e297d2f66788d419a607c
Brian Kingery <briankingery87@gmail.com>
2016-10-13 11:42:27 -0400

    Add files via upload

---
+++ ProjectFiles/Scripts/QuickCreate_AddIn.py
@@ -0,0 +1,7 @@
+import webbrowser,time
+
+webbrowser.open(r'R:\Divisions\InfoTech\Shared\GIS\Python_Addins\addin_assistant\Projects\MyTools\NNWW_Toolbars\makeaddin.py')
+time.sleep(3)
+webbrowser.open(r'R:\Divisions\InfoTech\Shared\GIS\Python_Addins\addin_assistant\Projects\MyTools\NNWW_Toolbars\NNWW_Toolbars.esriaddin')
+time.sleep(5)
+webbrowser.open(r'C:\Program Files (x86)\ArcGIS\Desktop10.2\bin\ArcMap.exe')
